2a2c0ef653add8bc383b91b494cda8ef49fc24b8
Yookdong <ydhydh45@gmail.com>
2023-09-13 14:31:04 +0900

    Build.cs 수정

--- Source/TeamProject/TeamProject.Build.cs
+++ Source/TeamProject/TeamProject.Build.cs
@@ -9,6 +9,15 @@ public class TeamProject : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "UMG", "Paper2D", "GameplayTasks" });
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 
         // Include 시 경로를 생략
 
